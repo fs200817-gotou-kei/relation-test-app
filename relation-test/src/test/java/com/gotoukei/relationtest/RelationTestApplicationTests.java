9e8baa1203060bf7503857d641356647c0feb9a5
fs200817-gotou-kei <k_goto@freestyles.jp>
2022-11-16 07:12:25 +0900

    first commit

---
+++ relation-test/src/test/java/com/gotoukei/relationtest/RelationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotoukei.relationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
